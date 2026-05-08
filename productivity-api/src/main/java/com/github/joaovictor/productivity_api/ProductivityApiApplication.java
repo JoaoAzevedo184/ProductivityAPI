362fd5a1c962c2ea9b4c0774c78d7bab02413ccf
JoaoAzevedo184 <ccjconrad50@gmail.com>
2026-05-08 20:30:54 -0300

    Initial commit

---
+++ productivity-api/src/main/java/com/github/joaovictor/productivity_api/ProductivityApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.joaovictor.productivity_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductivityApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductivityApiApplication.class, args);
+	}
+
+}
